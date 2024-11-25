5d109394b7d8b25ec7a82945cbdfa212809ee15f
axel.perales <axel.perales@meinberg-usa.com>
2024-11-25 14:38:04 -0800

    Plug-in takes a sample ICD file and extracts the Communication, selected IED, and DataTypeTemplates sections

--- mbg-icd-extractor.js
+++ mbg-icd-extractor.js
@@ -1,5 +1,53 @@
 import { LitElement, html } from 'lit';
 import '@material/web/dialog/dialog.js';
+import '@material/web/list/list.js';
+import '@material/web/list/list-item.js';
+
+function extractCommunication(ied) {
+  const comm = ied.ownerDocument.querySelector(':root>Communication');
+  const connAPs = Array.from(
+    ied.ownerDocument.querySelectorAll(
+      `:root>Communication>SubNetwork>ConnectedAP[iedName="${ied.getAttribute('name')}"]`,
+    ),
+  );
+  connAPs.forEach(connAP => {
+    const subnet = connAP.closest('SubNetwork');
+    let clonedSubnet = comm.querySelector(
+      `SubNetwork[name="${subnet.getAttribute('name')}"]`,
+    );
+    if (!clonedSubnet) {
+      clonedSubnet = subnet.cloneNode(false);
+      comm.appendChild(clonedSubnet);
+    }
+    const clonedConnAP = connAP.cloneNode(true);
+    clonedSubnet.appendChild(clonedConnAP);
+  });
+  return comm;
+}
+
+function extractIED(ied) {
+  const doc = document.implementation.createDocument(
+    'http://www.iec.ch/61850/2003/SCL',
+    'SCL',
+  );
+  doc.documentElement.appendChild(extractCommunication(ied));
+  doc.documentElement.appendChild(ied.cloneNode(true));
+  doc.documentElement.appendChild(
+    ied.ownerDocument.querySelector(':root>DataTypeTemplates')?.cloneNode(true),
+  );
+  // TODO: wrap the XML doc with a pretty-printed version
+  return new XMLSerializer().serializeToString(doc);
+}
+
+function downloadIED(ied) {
+  const hiddenElement = document.createElement('a');
+  hiddenElement.href = `data:application/xml,${encodeURI(extractIED(ied))}`;
+  hiddenElement.target = '_blank';
+  hiddenElement.download = `${ied.getAttribute('name')}.xml`;
+  document.body.appendChild(hiddenElement);
+  hiddenElement.click();
+  document.body.removeChild(hiddenElement);
+}
 
 export default class MbgIcdExtractor extends LitElement {
   static properties = {
@@ -13,37 +61,17 @@ export default class MbgIcdExtractor extends LitElement {
   render() {
     return html`
       <md-dialog>
-        <div slot="headline">Choose your favorite pet</div>
-        <form id="form" slot="content" method="dialog">
-          <label>
-            <md-radio
-              name="pet"
-              value="cats"
-              aria-label="Cats"
-              touch-target="wrapper"
-              checked
-            ></md-radio>
-            <span aria-hidden="true">Cats</span>
-          </label>
-          <label>
-            <md-radio
-              name="pet"
-              value="dogs"
-              aria-label="Dogs"
-              touch-target="wrapper"
-            ></md-radio>
-            <span aria-hidden="true">Dogs</span>
-          </label>
-          <label>
-            <md-radio
-              name="pet"
-              value="birds"
-              aria-label="Birds"
-              touch-target="wrapper"
-            ></md-radio>
-            <span aria-hidden="true">Birds</span>
-          </label>
-        </form>
+        <div slot="headline">Choose the IED</div>
+        <md-list slot="content">
+          ${Array.from(
+            this.doc?.querySelectorAll('IED[manufacturer^="Meinberg"]'),
+          ).map(
+            ied =>
+              html`<md-list-item type="button" @click=${() => downloadIED(ied)}
+                >${ied.getAttribute('name')}</md-list-item
+              >`,
+          )}
+        </md-list>
         <div slot="actions">
           <md-text-button form="form" value="cancel">Cancel</md-text-button>
           <md-text-button form="form" value="ok">OK</md-text-button>
